989902dbf9fab99b7a7ff4043fd2c2a74ecac5a0
Athaobyr <37582310+Athaobyr@users.noreply.github.com>
2021-06-08 13:56:17 -0300

    Add files via upload

--- class/sql.php
+++ class/sql.php
@@ -1,32 +1,52 @@
-<?php
-    class Sql extends PDO{
+<?php 
 
-        private $conn;
-        public function __construct()
-        {
-            $this->conn = new PDO("mysql:dbname=dbphp7;local=localhost","root","");
-        }
-    
-        private function setParams($statment, $parameters = array()){
-            foreach ($parameters as $key => $value){
-                $this->setParam($key,$value);
-            }
-        }
+class Sql extends PDO {
 
-        private function setParam($statment, $key, $value){
-            $statment->bindParam($key,$value);
-        }
-        
-        public function query($rawQuery, $params = array()){
-            $stmt = $this->conn->prepare($rawQuery);
-            $this->setParams($stmt, $params);
-            $stmt->execute();
-            return $stmt;
-        }
+	private $conn;
 
-        public function select($rawQuery, $params = array()):array {
-            $stmt = $this->query($rawQuery, $params);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-    }
-?>
+	public function __construct(){
+
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
+	}
+
+	private function setParams($statement, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function setParam($statement, $key, $value){
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array()){
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
